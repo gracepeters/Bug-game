4ef8dc25aad7022f95659eb5891ea44a27be17c9
Grace Peters <grace.peters20@calhoun.org>
2018-12-20 17:55:15 +0000

    adds reset button:

--- oo.js
+++ oo.js
@@ -2,65 +2,82 @@ let color1 = 0
 let color2 = 0
 var character;
 var circle;
-let lives= 3;
-let score =0;
+let lives = 3;
+let score = 0;
+var button;
+
 
 function setup() {
-    createCanvas(windowWidth,windowHeight)
+    createCanvas(windowWidth, windowHeight)
     colorMode(HSB, 360, 100, 100)
     //initialize every instance of the ball class
     ballArray = []
-    for(i=0; i < 30; i++){
-        ballArray.push(new Ball(random(10,50)))
+    for (i = 0; i < 30; i++) {
+        ballArray.push(new Ball(random(10, 50)))
     }
-    explosionArray=[]
-    character= new Character
-    circle=new Circle
-    let score= 0;
+    explosionArray = []
+    character = new Character
+    circle = new Circle
+    let score = 0;
     textSize(30);
+    button = createButton('Play again');
+    button.position(windowWidth/2, windowHeight/2);
+    button.size(100,20,10)
+    button.mouseClicked(clickFunction);
+    button.hide()
+    }
+
+function clickFunction(){
+    score=0;
+    lives=3;
+    for (i = 0; i < 30; i++) {
+        ballArray.push(new Ball(random(10, 50)))
+    button.hide()
+    }
 }
 
 function draw() {
-    background(220,0,80)
-    ballArray.forEach( ball =>{
+    background(220, 0, 80)
+    ballArray.forEach(ball => {
         ball.display()
         ball.moveBall()
         ball.checkCollision()
     })
-    explosionArray.forEach(explosion =>{
+    explosionArray.forEach(explosion => {
         explosion.display()
         explosion.shrink()
     })
     circle.display()
-    if(mouseIsPressed){
-        console.log("circle should grow")
-    circle.growCircle()
+    if (mouseIsPressed) {
+        circle.growCircle()
     }
     character.display()
-    fill(0,0,0)
-    text("Score: "+ score, 1060,40)
-    text("Lives: "+ lives,1250,40)
-    
-    if(lives<=0){
-        console.log(lives)
-        text("GAME OVER", width/2.2,height/2.2)
-        ball.Array = []
-        button = createButton('Play again');
-        button.position(100, 65);
-        button.mousePressed();
+    fill(0, 0, 0)
+    text("Score: " + score, 1060, 40)
+    text("Lives: " + lives, 1250, 40)
+
+    if (lives <= 0) {
+        text("GAME OVER", width / 2.15, height / 2.1)
+        ballArray = []
+        button.show()
+        // button = createButton('Play again');
+        //button.position(100, 65);
+        //button.mousePressed();
     }
 }
 
+
+
 //function mouseClicked(){
- //   ballArray.push(new Ball(random(5,70)))
- //   console.log("ball")
+//   ballArray.push(new Ball(random(5,70)))
+//   console.log("ball")
 //}
-function mouseReleased(){
-        circle.diameter= 30;
-        //setTimeout(() => 
-        explosionArray.push(new Explosion())
+function mouseReleased() {
+    circle.diameter = 30;
+    //setTimeout(() => 
+    explosionArray.push(new Explosion())
 }
- 
+
 
 
 class Ball {
@@ -68,17 +85,17 @@ class Ball {
     //in this case, properties are: x, y, diameter, xVel, yVel
     constructor(diameter) {
         this.diameter = diameter
-        if (mouseX <= 0 || mouseY<= 0){
-            this.x = random(this.diameter, 1426-this.diameter)
-            this.y = random(this.diameter,732-this.diameter)
+        if (mouseX <= 0 || mouseY <= 0) {
+            this.x = random(this.diameter, 1426 - this.diameter)
+            this.y = random(this.diameter, 732 - this.diameter)
         }
-        else{
-            this.x=random(width)
-            this.y=random(height)
+        else {
+            this.x = random(width)
+            this.y = random(height)
         }
-        this.xVelocity = random(1-5)
-        this.yVelocity = random(1-5)
-        this.color= random(360)
+        this.xVelocity = random(1 - 5)
+        this.yVelocity = random(1 - 5)
+        this.color = random(360)
     }
 
     //methods to control the changes to the object
@@ -90,96 +107,96 @@ class Ball {
     moveBall() {
         this.x += this.xVelocity
         this.y += this.yVelocity
-        if (this.x+ this.diameter/2 > width || this.x-this.diameter/2 < 0) {
+        if (this.x + this.diameter / 2 > width || this.x - this.diameter / 2 < 0) {
             this.xVelocity *= -1
         }
         if (this.y > height || this.y < 0) {
             this.yVelocity *= -1
         }
     }
-    
-    checkCollision(){
-        if (collideCircleCircle(this.x, this.y, this.diameter,mouseX,mouseY,circle.diameter)){
+
+    checkCollision() {
+        if (collideCircleCircle(this.x, this.y, this.diameter, mouseX, mouseY, circle.diameter)) {
             console.log("collison detected, shrinking circle")
-            circle.diameter= 30;
-            lives -=1;
+            circle.diameter = 30;
+            lives -= 1;
         }
-        if(explosionArray.length>0){
-            let mre = explosionArray[explosionArray.length-1]
-            if(collideCircleCircle(this.x, this.y, this.diameter, mre.x, mre.y, mre.diameter)){
+        if (explosionArray.length > 0) {
+            let mre = explosionArray[explosionArray.length - 1]
+            if (collideCircleCircle(this.x, this.y, this.diameter, mre.x, mre.y, mre.diameter)) {
                 console.log("explo and circle collide")
-                ballArray.splice(ballArray.indexOf(this),1)
-                ballArray.push(new Ball(random(10,50)))
-                score+=100;
+                ballArray.splice(ballArray.indexOf(this), 1)
+                ballArray.push(new Ball(random(10, 50)))
+                score += 100;
             }
         }
     }
-    
-    
+
+
 }
 class Character {
-    constructor(){
-        this.color = color(0,100,0);
-        this.x=100;
-        this.y=100;
+    constructor() {
+        this.color = color(0, 100, 0);
+        this.x = 100;
+        this.y = 100;
         this.width = 30;
         this.height = 50;
         this.diameter = 60;
-        this.velocity=4;
+        this.velocity = 4;
     }
-    display(){
+    display() {
         fill(this.color);
         noStroke();
-        rect(mouseX-(this.width/2), mouseY-(this.height/2), this.width, this.height)
+        rect(mouseX - (this.width / 2), mouseY - (this.height / 2), this.width, this.height)
     }
-    moveCharacter(){
-        this.x+=this.velocity
-        if(this.x>=800){
-            this.x=0
+    moveCharacter() {
+        this.x += this.velocity
+        if (this.x >= 800) {
+            this.x = 0
         }
     }
 }
 
 
-class Circle{
-    constructor(){
-        this.color = color(0,100,0);
-        this.x=500;
-        this.y=500;
+class Circle {
+    constructor() {
+        this.color = color(0, 100, 0);
+        this.x = 500;
+        this.y = 500;
         this.width = 79.9;
-        this.height =99.9;
+        this.height = 99.9;
         this.diameter = 30;
-        this.velocity=4;
+        this.velocity = 4;
     }
-    display(){
+    display() {
         fill(this.color);
-        ellipse(mouseX,mouseY,this.diameter)
+        ellipse(mouseX, mouseY, this.diameter)
     }
-    growCircle(){
-        this.diameter+=3;
-        this.width +=2;
+    growCircle() {
+        this.diameter += 3;
+        this.width += 2;
         this.height += 3;
     }
-    
+
 }
 
-class Explosion{
-    constructor(){
-        this.color= color(0,100,0);
-        this.x=mouseX;
-        this.y=mouseY;
-        this.diameter=circle.diameter*10;
+class Explosion {
+    constructor() {
+        this.color = color(0, 100, 0);
+        this.x = mouseX;
+        this.y = mouseY;
+        this.diameter = circle.diameter * 10;
     }
-    display(){
+    display() {
         fill(this.color);
-        ellipse(this.x,this.y,this.diameter)
+        ellipse(this.x, this.y, this.diameter)
     }
-    shrink(){
-        if(this.diameter>0){
-            this.diameter-=10;
+    shrink() {
+        if (this.diameter > 0) {
+            this.diameter -= 10;
         }
-        else{
-            explosionArray.splice(explosionArray.indexOf(this),1);
+        else {
+            explosionArray.splice(explosionArray.indexOf(this), 1);
         }
     }
 }
